b3806706d93b72e907de61628e0364dca7092636
takagi-135 <sa5ra12sa@gmail.com>
2024-07-18 16:43:08 +0900

    テストコミット

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-10">
+      <div className="h-20 m-0 bg-red-300"></div>
+    </main>
   );
 }
