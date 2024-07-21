3ca81dc3a8ecae8821296fd0ef3efe2edc9f2d5b
takagi-135 <sa5ra12sa@gmail.com>
2024-07-22 08:45:17 +0900

    プレリクにてご指摘いただいた箇所を修正いたしました。

--- src/components/main/top-image/index.tsx
+++ src/components/main/top-image/index.tsx
@@ -5,10 +5,11 @@ export default function TopPic() {
     <>
       <div className="fixed w-fit -z-50">
         <Image
-          src="/top.jpg"
+          src="/high-quality-printing.jpg"
           alt="高品質な印刷を提供。職人がお届けする、美しい製品。"
           width={1200}
           height={700}
+          priority={true}
         />
       </div>
     </>
