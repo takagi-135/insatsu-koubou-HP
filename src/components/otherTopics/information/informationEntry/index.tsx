1ccac591d1aa0eacc07a96548e80082803e33952
takagi-135 <sa5ra12sa@gmail.com>
2024-09-17 15:56:56 +0900

    反映漏れを修正しました。

--- src/components/otherTopics/information/informationEntry/index.tsx
+++ src/components/otherTopics/information/informationEntry/index.tsx
@@ -5,12 +5,12 @@ export default function InformationEntry() {
     <div className="m-0 h-[140px] w-[380px] bg-primary">
       {INFORMATION_ENTRY.map((informationEntry) => {
         return (
-          <>
+          <div key={informationEntry.id}>
             <p className="pl-2 pt-1 text-sm text-secondary">
               {informationEntry.date}
             </p>
             <p className="text-md pb-1 pl-2">{informationEntry.title}</p>
-          </>
+          </div>
         );
       })}
     </div>
