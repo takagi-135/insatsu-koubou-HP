8b9f02a63560dd1e6524a21a6519ba3988b82d7f
MatsunamiDaiki <lax.md0@gmail.com>
2024-10-13 12:19:59 +0900

    ページ用の変数追加

---
+++ src/constants/pages.ts
@@ -0,0 +1,13 @@
+const BASE_PATHS = {
+  top: '/',
+  presidentMessage: '/president-message',
+};
+
+export const PAGES = {
+  top: {
+    root: BASE_PATHS.top,
+  },
+  presidentMessage: {
+    root: BASE_PATHS.presidentMessage,
+  },
+};
