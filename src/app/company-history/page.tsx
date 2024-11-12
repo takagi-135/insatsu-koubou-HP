390883c5a1463e8dc90d0914878181037fd99a5a
chat-howaro <achahowaro0127@gmail.com>
2024-11-12 11:20:26 +0900

    沿革ページ　実装
    ご確認よろしくお願いします。

--- src/app/company-history/page.tsx
+++ src/app/company-history/page.tsx
@@ -1,3 +1,109 @@
 export default function CompanyHistoryPage() {
-  return <div>沿革ページ</div>;
+  return (
+    <>
+      <div className="relative max-w-[950px] mx-auto pt-[150px] pb-[50px] p-4 z-20 text-[rgb(90,62,25)]">
+        <header>
+          <h2 className="text-3xl font-bold mt-6 leading-[2]">HISTORY</h2>
+          <h3 className="text-lg font-semibold">沿革</h3>
+        </header>
+
+
+        <section className="mt-12 relative">
+          <div className="absolute left-[56px] top-3 bottom-3 w-1 bg-orange-500 hidden lg:block"></div>
+
+          <div className="flex flex-col gap-y-10 pl-10">
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1955</div>
+              <div className="w-3/4 font-bold">
+                松波忠正、岐阜市鍵屋西町2丁目15番地にて松波印刷所を創立
+              </div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1960</div>
+              <div className="w-3/4 font-bold">
+                社名を有限会社松波印刷と改め、松波忠正、代表取締役社長に就任
+              </div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1973</div>
+              <div className="w-3/4 font-bold">岐阜市黒野南4丁目168番地に新社屋完成</div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1974</div>
+              <div className="w-3/4 font-bold">
+                上記住所へ本社を移転。同時にオフセット印刷、製版、製本工場を増設
+              </div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1983</div>
+              <div className="w-3/4 font-bold">
+                松波印刷株式会社と改称。カラー印刷部門の強化をはかる
+              </div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1988</div>
+              <div className="w-3/4 font-bold">本社・工場の増築・改修工事を完成</div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1989</div>
+              <div className="w-3/4 font-bold">
+                自社製版システムを確立。同時に電算写植を導入
+              </div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">1997</div>
+              <div className="w-3/4 font-bold">
+                岐阜県揖斐郡大野町下磯750番地の1に本社・工場を新築、移転
+              </div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">2002</div>
+              <div className="w-3/4 font-bold">松波茂樹、代表取締役社長に就任</div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">2004</div>
+              <div className="w-3/4 font-bold">中綴機導入、製本部門の強化をはかる</div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">2005</div>
+              <div className="w-3/4 font-bold">無線綴機導入</div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">2007</div>
+              <div className="w-3/4 font-bold">プライバシーマーク（Pマーク）取得</div>
+            </div>
+
+            <div className="flex relative">
+              <div className="absolute left-[10px] top-1/2 transform -translate-y-1/2 w-4 h-4 rounded-full bg-orange-500 hidden lg:block"></div>
+              <div className="w-1/4 text-center pr-20 font-bold">2017</div>
+              <div className="w-3/4 font-bold">株式会社印刷工房と改称</div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
